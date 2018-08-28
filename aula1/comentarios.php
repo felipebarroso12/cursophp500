75f71b08e79c8783e23cb4bf21ee195916e172d3
Felipe Barroso <felipe-vendas@msn.com>
2018-08-28 19:08:52 -0300

    atquivos inicais do projeto

---
+++ aula1/comentarios.php
@@ -0,0 +1,38 @@
+<style>
+	h1{
+		color:#2b2929;
+		font-family: helvetica;
+		text-transform: uppercase;
+		font-size: 18px;
+	}
+	p{
+		font-size: 16px;
+		font-family: helvetica;
+	}
+</style>
+<?php 
+	# comentario simples de apenas uma linhas
+	# print date();
+
+	// comentario simples de apenas uma linha
+	// print date();
+
+	/*
+	 * O comando abaixo precisou ser comentado
+	 * Porque estamos aprendendo utilizar comentários
+	 *corretamento do PHP.
+	 */
+	/* print date(); */
+
+	/**
+	 * Variavel para armazenar a marca do veículo
+	 * Obs: utilize este tipo de comentário quando for
+	 * documentar variáveis, funções parametros e etc.
+	 *
+	 * @var $marca string
+	 */
+	 $marca = 'GM'; 
+?>
+<script type="text/javascript">
+	console.log('Ola')
+</script>
---
+++ aula1/tags.php
@@ -0,0 +1,20 @@
+<style>
+	h1{
+		color:#2b2929;
+		font-family: helvetica;
+		text-transform: uppercase;
+		font-size: 18px;
+	}
+	p{
+		font-size: 16px;
+		font-family: helvetica;
+	}
+</style>
+<h1> Turma - 6523 </h1>
+<p><?=	'1º forma de echo';	?></p> <!-- NOVA FORMA DE FAZER ECHO -->
+<?php
+	echo '<p>2º forma de echo</p>'
+?>
+<script type="text/javascript">
+	console.log('Ola')
+</script>
